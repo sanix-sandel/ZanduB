ef7a49a1c0963d890858d9bc685e077c8321d44c
sanix sandel <sanicksikani@gmail.com>
2020-06-06 10:15:21 +0300

    order

--- zandu/orders/models.py
+++ zandu/orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from products.models import Product
+from django.conf import settings
 
-# Create your models here.
+class Order(models.Model):
+    customer=models.ForeignKey(settings.AUTH_USER_MODEL,
+                                related_name='order',
+                                on_delete=models.CASCADE)
+
+    created=models.DateTimeField(auto_add_now=True)
+    updated=models.BooleanField(default=False)
+    paid=models.BooleanField(default=False)
+
+    class Meta:
+        ordering=('-created',)
+
+    def __str__(self):
+        return f"order by {self.customer.username}"
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order, related_name='items',
+                            on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, related_name='order_items',
+                            on_delete=models.CASCADE)
+    price=models.DeciamlField(max_digits=10, decimal_places=2)
+    quantity=models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"order of {self.product.title}"
+
+    def get_cost(self):
+        return self.price*self.quantity
