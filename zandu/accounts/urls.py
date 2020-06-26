4e5fa0f7e451ac95a947105ab28a81bc48b6829f
sanix sandel <sanicksikani@gmail.com>
2020-06-26 14:52:58 +0300

    profile view cached

--- zandu/accounts/urls.py
+++ zandu/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.views.decorators.cache import cache_page
+
 
 app_name='accounts'
 
 urlpatterns = [
-    path('profile/<uuid:pk>/', views.Update_Profile.as_view(), name='update_profile'),
-       
+    path('profile/<uuid:pk>/', cache_page(60*15)(views.Update_Profile.as_view()), name='update_profile'),   
 ]
