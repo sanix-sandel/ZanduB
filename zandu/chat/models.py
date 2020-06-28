dc3492d464c5d6ff3ab86a82f91a3f7ae4a2d2a5
sanix sandel <sanicksikani@gmail.com>
2020-06-28 13:33:58 +0300

    asy

--- zandu/chat/models.py
+++ zandu/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Message(models.Model):
+    content=models.TextField()
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, 
+        related_name='author', on_delete=models.CASCADE)
+    sent_on=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"message of {author.username}"
