3fbbe90fa2dd7389b0269111263ef7c38b86a48c
sanix sandel <sanicksikani@gmail.com>
2020-07-21 23:38:23 +0300

    Rest

--- products/serializers.py
+++ products/serializers.py
@@ -1,18 +1,28 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import*
+from accounts.serializers import UserSerializer
 
 class CategorySerializer(serializers.ModelSerializer):
-    
+   
+    product=serializers.ReadOnlyField(source='product.title')
     class Meta:
         model=Category
         fields='__all__'    
 
 class ProductSerializer(serializers.ModelSerializer):
-    category=CategorySerializer()
+   # category=CategorySerializer()
+    category=serializers.ReadOnlyField(source='category.name')#get the name
+    likes=UserSerializer(many=True)#nested
     class Meta:
         model=Product
         fields='__all__'
 
 class ProductActionSerializer(serializers.Serializer):
     id=serializers.UUIDField()        
+
+    """# product=serializers.HyperlinkedModelSerializer(
+    #    many=True,
+    #    read_only=True,
+    #    view_name='product_detail'
+    #)"""
--- stores/serializers.py
+++ stores/serializers.py
@@ -3,9 +3,14 @@ from rest_framework import serializers
 from .models import*
 from accounts.serializers import UserSerializer
 
+
 class StoreSerializer(serializers.ModelSerializer):
 
     owner=UserSerializer()
+    followers=UserSerializer(many=True, read_only=True)#nested
+   # followers=serializers.StringRelatedField(many=True)
+    #instead of StringRelatedField, PrimaryKeyrelatedField can be used
+    #so id will be displayed
     class Meta:
         model=Store
         fields=('id',
@@ -21,3 +26,7 @@ class StoreSerializer(serializers.ModelSerializer):
 
 class StoreActionSerializer(serializers.Serializer):
     id=serializers.UUIDField()        
+
+
+#SlugRelatedField may be used to represent the target of the relationship 
+# using a field on the target(id, title, name,....).
