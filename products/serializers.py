6cf9fb023b76a06d10c2e0401c1e59ac2bb2f1fb
sanix sandel <sanicksikani@gmail.com>
2020-07-21 20:54:44 +0300

    api config

--- products/serializers.py
+++ products/serializers.py
@@ -9,7 +9,7 @@ class CategorySerializer(serializers.ModelSerializer):
         fields='__all__'    
 
 class ProductSerializer(serializers.ModelSerializer):
-    
+    category=CategorySerializer()
     class Meta:
         model=Product
         fields='__all__'
--- stores/serializers.py
+++ stores/serializers.py
@@ -1,11 +1,23 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import*
+from accounts.serializers import UserSerializer
 
 class StoreSerializer(serializers.ModelSerializer):
+
+    owner=UserSerializer()
     class Meta:
         model=Store
-        fields='__all__'
+        fields=('id',
+            'title', 
+            'cover_image', 
+            'owner', 
+            'slogan', 
+            'about',
+            'rate',
+            'address',
+            'date_created',
+            'followers')
 
 class StoreActionSerializer(serializers.Serializer):
     id=serializers.UUIDField()        
